1e6ef23b68d89dcc6709cffcc74232b2534cc4fd
lizhengang9 <lizhengang9@jd.com>
2020-09-12 20:01:35 +0800

    for of

--- stocklist.js
+++ stocklist.js
@@ -11,34 +11,53 @@ const arr = [
   // 'sz300136',
   'sz002600',
   'sh603986',
-  "sh600030", // zhongxin
-  "sh601066", // jiantou
-  "sz002241", // geer
-  'hk02382',  //shunyu
-  "sh688126", // hugui
+  'sh600030', // zhongxin
+  'sh601066', // jiantou
+  'sz002241', // geer
+  'hk02382', //shunyu
+  'sh688126', // hugui
   // "sh600703", // sanan
-  "sh603983", // wanmei
-  "sh601628", // renshou
+  'sh603983', // wanmei
+  'sh601628', // renshou
 ];
 
-function fetch(url) {
-  request({ url: url, encoding: null }, (err, response, body) => {
-    try {
-      const data = iconv.decode(body, 'gb2312');
-      const list = data.toString('utf8').split('~');
-      console.log(list[1], list[3], list[32] + '%');
-    } catch (error) {}
-  });
-}
+const fetch = (num) => {
+  return new Promise((resolve, reject) => {
+    const url = `http://qt.gtimg.cn/q=${num}`;
+    request({ url: url, encoding: null }, (err, response, body) => {
+      try {
+        const data = iconv.decode(body, 'gb2312');
+        const list = data.toString('utf8').split('~');
+        console.log(list[1], list[3], list[32] + '%');
 
-function query() {
-  try {
-    arr.forEach((ele) => {
-      const url = `http://qt.gtimg.cn/q=${ele}`;
-      fetch(url);
+        resolve();
+      } catch (error) {
+        // reject(error);
+      }
     });
-  } catch (error) {
-    // console.log('error', error);
+  });
+};
+
+async function query() {
+  // try {
+  //   arr.forEach(async ele => {
+  //     const url = `http://qt.gtimg.cn/q=${ele}`;
+  //     await fetch(url);
+  //   });
+  // } catch (error) {
+  //   // console.log('error', error);
+  // }
+
+  // const promiseArr = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   promiseArr.push(fetch(arr[i]));
+  // }
+  // Promise.all(promiseArr).then((rs) => {
+  //   // console.log('finish');
+  // });
+
+  for(let x of arr) {
+    await fetch(x);
   }
 }
 
